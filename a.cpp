2843e6abb31ffb95d5ac2bf29e53b200e5679c56
xjoi <2747622257@qq.com>
2018-07-11 12:27:22 +0800

    add new files

---
+++ a.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll long long
+const int N=10005;
+using namespace std;
+
+int len;
+char c1[N],c2[N];
+
+int main(){
+	scanf("%s",c1+1);len=strlen(c1+1);
+	scanf("%s",c2+1);
+	printf("%s\n%s",c1+1,c2+1);
+}
